4e6d0edd0eddd334489abfa5ad12b7fb9269e7b8
allelaok <lasolim@wedigitize.com>
2022-04-20 18:53:32 +0900

    0420
    벡터값 문자열로 벡터 만들기 진행중

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,33 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string str = "[1,2,3],[4,5,6],[7,8,9]";
+            StrToVector(str);
+            Console.ReadLine();
+        }
+
+        static void StrToVector(string str)
+        {
+            
+            string[] joints = str.Split(']');
+
+            for (int i = 0; i < joints.Length; i++)
+            {
+                //joints[i].Trim('[');
+
+                //Console.WriteLine(joints[i].Trim('[').Trim(',').Trim('['));
+
+                string[] component = joints[i].Trim('[').Trim(',').Trim('[').Split(',');
+
+                for (int j = 0; j < component.Length; j++)
+                {
+                    if (Int32.TryParse(component[j], out int a))
+                        Console.WriteLine(a);
+                    else
+                        Console.WriteLine("String could not be parsed. //" + component[j] + "//" + j);
+                }
+
+            }
         }
     }
 }
